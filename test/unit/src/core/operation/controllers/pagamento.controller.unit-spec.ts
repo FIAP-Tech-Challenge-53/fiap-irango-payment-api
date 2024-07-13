d172517fe9787dbe8fc354600140606d9bb6bdc8
Caio <caio.cb.sp@gmail.com>
2024-07-13 18:00:20 -0300

    Unit Tests for Controllers

--- test/unit/src/core/operation/controllers/pagamento.controller.unit-spec.ts
+++ test/unit/src/core/operation/controllers/pagamento.controller.unit-spec.ts
@@ -1,18 +1,85 @@
-/*import UpdatePagamento from '@/core/application/usecase/pagamento/update-pagamento.use-case'
+import UpdatePagamento from '@/core/application/usecase/pagamento/update-pagamento.use-case'
 import PagamentoDto from '@/core/domain/dto/output/pagamento.dto'
 import PagamentoMapper from '@/core/domain/mappers/pagamento.mapper'
 import IPagamentoRepository from '@/core/domain/repositories/ipagamento.repository'
 import IGatewayPagamentoService from '@/core/domain/services/igateway-pagamento.service'
-import { PagamentoGateway } from '@/core/operation/gateway/pagamento.gateway'
 import IRangoOrderService from '@/infra/persistence/service/irango-order.service'
-import { PagamentoController } from '@/core/operation/controllers/pagamento.controller'*/
+import { PagamentoController } from '@/core/operation/controllers/pagamento.controller'
+import Pagamento from '@/core/domain/entities/pagamento'
+import { PagamentoStatusEnum } from '@/core/domain/enums/pagamento-status.enum'
 
 describe("Test PagamentoController class", () => {
-    it("constructor class test", async () => {
 
+    let controller:PagamentoController;
+
+    let mockOrderService:jest.Mocked<IRangoOrderService>;
+    let mockPagamentoRepository:jest.Mocked<IPagamentoRepository>;
+    let mockPagamentoService:jest.Mocked<IGatewayPagamentoService>;
+
+    let mockUpdatePagamentoHandler:jest.Mock<any>;
+    let mockPagamentoMapper:jest.Mock<any>;
+
+    beforeEach(() => {   
+        
+        mockUpdatePagamentoHandler = jest.fn()
+        mockPagamentoMapper = jest.fn()
+
+        mockPagamentoRepository = {
+            findByPedidoId: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn()
+        };
+
+        mockPagamentoService = {
+            registerOrder: jest.fn()
+        };
+
+        mockOrderService = {
+            confirmPayment: jest.fn()
+        };
+
+        UpdatePagamento.prototype.handle = mockUpdatePagamentoHandler;
+        PagamentoMapper.toDto = mockPagamentoMapper;
+
+        controller = new PagamentoController(mockPagamentoRepository, mockPagamentoService, mockOrderService );
+    });
+
+    it("constructor class test", async () => {
+        expect(controller).toBeInstanceOf(PagamentoController);
     });
 
     it("handleWebhookMercadoPago class method test", async () => {
+        const pagamento = new Pagamento({
+            id: '1',
+            pedidoId: 1,
+            valor: 1,
+            gatewayPagamentoId: '1',
+            status: PagamentoStatusEnum.PENDENTE,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        });
+
+        const dto: PagamentoDto = {
+            id: '1',
+            pedidoId: 1,
+            valor: 1,
+            gatewayPagamentoId: '1',
+            status: PagamentoStatusEnum.PENDENTE,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        };
+
+        mockUpdatePagamentoHandler.mockResolvedValue(pagamento);
+        mockPagamentoMapper.mockReturnValue(dto);
+
+        let result = await controller.handleWebhookMercadoPago(1, false);
+
+        expect(mockUpdatePagamentoHandler).toHaveBeenCalledTimes(1)
+        expect(mockPagamentoMapper).toHaveBeenCalledTimes(1)
+
+        expect(mockUpdatePagamentoHandler).toHaveBeenCalledWith(1, false)
+        expect(mockPagamentoMapper).toHaveBeenCalledWith(pagamento)
+        expect(result).toEqual(dto)
 
     });
 });
--- test/unit/src/core/operation/controllers/pedido.controller.unit-spec.ts
+++ test/unit/src/core/operation/controllers/pedido.controller.unit-spec.ts
@@ -1,21 +1,86 @@
-/*import Register from '@/core/application/usecase/pedido/register.use-case'
-import PedidoDto from '@/core/domain/dto/output/pedido.dto'
+import Register from '@/core/application/usecase/pedido/register.use-case'
 import PedidoMapper from '@/core/domain/mappers/pedido.mapper'
 import IPagamentoRepository from '@/core/domain/repositories/ipagamento.repository'
 import IPedidoRepository from '@/core/domain/repositories/ipedido.repository'
 import IGatewayPagamentoService from '@/core/domain/services/igateway-pagamento.service'
-import { PagamentoGateway } from '@/core/operation/gateway/pagamento.gateway'
-import { PedidoGateway } from '@/core/operation/gateway/pedido.gateway'
 import RegisterPedidoRequest from '@/infra/web/nestjs/pedidos/dto/register-pedido.request'
-import { PedidoController } from '@/core/operation/controllers/pedido.controller'*/
+import { PedidoController } from '@/core/operation/controllers/pedido.controller'
+import Pedido from '@/core/domain/entities/pedido'
 
 describe("Test for ProdutoController Class", () => {
 
-    it("constructor class test", async () => {
+    let controller:PedidoController;
 
+    let mockPedidoRepository:jest.Mocked<IPedidoRepository>;
+    let mockPagamentoRepository:jest.Mocked<IPagamentoRepository>;
+    let mockPagamentoService:jest.Mocked<IGatewayPagamentoService>;
+
+    let mockRegisterHandler:jest.Mock<any>;
+    let mockDto:jest.Mock<any>;
+
+    beforeEach(() => {   
+        
+        mockRegisterHandler = jest.fn()
+        mockDto = jest.fn()
+
+        mockPagamentoRepository = {
+            findByPedidoId: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn()
+        };
+
+        mockPagamentoService = {
+            registerOrder: jest.fn()
+        };
+
+        mockPedidoRepository = {
+            create: jest.fn(),
+            save: jest.fn()
+        };
+
+        Register.prototype.handle = mockRegisterHandler;
+        PedidoMapper.toDto = mockDto;
+
+        controller = new PedidoController(mockPedidoRepository,mockPagamentoRepository,mockPagamentoService);
+    });
+
+
+    it("constructor class test", async () => {
+        expect(controller).toBeInstanceOf(PedidoController);
     });
 
     it("register method test", async () => {
+        const input = new RegisterPedidoRequest();
+
+        const dto = {
+            id: 1,
+            consumidorId: '1',
+            total: 1,
+            pagamentoId: '1',
+            createdAt: new Date(1),
+            updatedAt: new Date(1)   
+        };
+
+        let pedido = new Pedido({
+            id: 1,
+            consumidorId: '1',
+            total: 1,
+            pagamentoId: '1',
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        });
+
+        mockRegisterHandler.mockResolvedValue(pedido);
+        mockDto.mockReturnValue(dto);
+
+        let result = await controller.register(input);
+
+        expect(mockRegisterHandler).toHaveBeenCalledTimes(1)
+        expect(mockDto).toHaveBeenCalledTimes(1)
+
+        expect(mockRegisterHandler).toHaveBeenCalledWith(input)
+        expect(mockDto).toHaveBeenCalledWith(pedido)
+        expect(result).toEqual(dto);
 
     });
 });
