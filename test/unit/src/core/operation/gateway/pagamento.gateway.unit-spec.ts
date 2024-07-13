26120816d4b78d951bd5c94fb41ae15961e1d4a2
Caio <caio.cb.sp@gmail.com>
2024-07-13 18:15:34 -0300

    Unit Tests for Gateway

--- test/unit/src/core/operation/gateway/pagamento.gateway.unit-spec.ts
+++ test/unit/src/core/operation/gateway/pagamento.gateway.unit-spec.ts
@@ -1,28 +1,69 @@
-/*import Pagamento from '@/core/domain/entities/pagamento'
+import Pagamento from '@/core/domain/entities/pagamento'
 import Pedido from '@/core/domain/entities/pedido'
 import IPagamentoRepository from '@/core/domain/repositories/ipagamento.repository'
 import IGatewayPagamentoService from '@/core/domain/services/igateway-pagamento.service'
-import { PagamentoGateway } from '@/core/operation/gateway/pagamento.gateway'*/
+import { PagamentoGateway } from '@/core/operation/gateway/pagamento.gateway'
 
 describe("Test PagamentoGateway class", () => {
-    it("constructor class test", async () => {
 
+    let gateway:PagamentoGateway;
+
+    let mockPagamentoRepository:jest.Mocked<IPagamentoRepository>;
+    let mockPagamentoService:jest.Mocked<IGatewayPagamentoService>;
+
+    beforeEach(() => {   
+
+        mockPagamentoRepository = {
+            findByPedidoId: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn()
+        };
+
+        mockPagamentoService = {
+            registerOrder: jest.fn()
+        };
+
+        gateway = new PagamentoGateway(mockPagamentoRepository, mockPagamentoService );
+    });
+
+    it("constructor class test", async () => {
+        expect(gateway).toBeInstanceOf(PagamentoGateway);
     });
 
     it("create method test", async () => {
-
+        const pagamento = new Pagamento();
+        mockPagamentoRepository.create.mockResolvedValue(pagamento);
+        let result = await gateway.create(pagamento);
+        expect(mockPagamentoRepository.create).toHaveBeenCalledTimes(1)
+        expect(mockPagamentoRepository.create).toHaveBeenCalledWith(pagamento)
+        expect(result).toEqual(pagamento)
     });
 
     it("registerOrder method test", async () => {
-
+        const pedido = new Pedido();
+        mockPagamentoService.registerOrder.mockResolvedValue("test");
+        let result = await gateway.registerOrder(pedido);
+        expect(mockPagamentoService.registerOrder).toHaveBeenCalledTimes(1)
+        expect(mockPagamentoService.registerOrder).toHaveBeenCalledWith(pedido)
+        expect(result).toEqual("test")
     });
 
     it("findByPedidoId method test", async () => {
-
+        const pagamento = new Pagamento();
+        mockPagamentoRepository.findByPedidoId.mockResolvedValue(pagamento);
+        let result = await gateway.findByPedidoId(1);
+        expect(mockPagamentoRepository.findByPedidoId).toHaveBeenCalledTimes(1)
+        expect(mockPagamentoRepository.findByPedidoId).toHaveBeenCalledWith(1)
+        expect(result).toEqual(pagamento)
     });
 
     it("save method test", async () => {
-
+        const pagamento = new Pagamento();
+        mockPagamentoRepository.save.mockResolvedValue(pagamento);
+        let result = await gateway.save(pagamento);
+        expect(mockPagamentoRepository.save).toHaveBeenCalledTimes(1)
+        expect(mockPagamentoRepository.save).toHaveBeenCalledWith(pagamento)
+        expect(result).toEqual(pagamento)
     });
 
 });
--- test/unit/src/core/operation/gateway/pedido.gateway.unit-spec.ts
+++ test/unit/src/core/operation/gateway/pedido.gateway.unit-spec.ts
@@ -1,18 +1,42 @@
-/*import Pedido from '@/core/domain/entities/pedido'
+import Pedido from '@/core/domain/entities/pedido'
 import IPedidoRepository from '@/core/domain/repositories/ipedido.repository'
-import { PedidoGateway } from '@/core/operation/gateway/pedido.gateway';*/
+import { PedidoGateway } from '@/core/operation/gateway/pedido.gateway';
 
 describe("Test PedidoGateway class", () => {
 
-    it("Testing class constructor", () => {
+    let gateway:PedidoGateway;
 
+    let mockPedidoRepository:jest.Mocked<IPedidoRepository>;
+
+    beforeEach(() => {   
+        
+        mockPedidoRepository = {
+            create: jest.fn(),
+            save: jest.fn()
+        };
+
+        gateway = new PedidoGateway(mockPedidoRepository);
+    });
+
+    it("Testing class constructor", () => {
+        expect(gateway).toBeInstanceOf(PedidoGateway);
     });
 
     it("create method test", async () => {
-
+        const pedido = new Pedido();
+        mockPedidoRepository.create.mockResolvedValue(pedido);
+        let result = await gateway.create(pedido);
+        expect(mockPedidoRepository.create).toHaveBeenCalledTimes(1)
+        expect(mockPedidoRepository.create).toHaveBeenCalledWith(pedido)
+        expect(result).toEqual(pedido)
     });
 
     it("save method test", async () => {
-
+        const pedido = new Pedido();
+        mockPedidoRepository.save.mockResolvedValue(pedido);
+        let result = await gateway.save(pedido);
+        expect(mockPedidoRepository.save).toHaveBeenCalledTimes(1)
+        expect(mockPedidoRepository.save).toHaveBeenCalledWith(pedido)
+        expect(result).toEqual(pedido)
     });
 });
