c17274cc6ee48ce5514038719890b8f471adbeff
Caio <caio.cb.sp@gmail.com>
2024-07-13 20:19:10 -0300

    Unit Tests for Persistence Services Classes

--- test/unit/src/infra/persistence/service/irango-order.service.unit-spec.ts
+++ test/unit/src/infra/persistence/service/irango-order.service.unit-spec.ts
@@ -1,15 +1,39 @@
-/*import axios from 'axios'
-
-import IOrderService from '@/core/domain/services/iorder.service'
+import axios from 'axios'
 import { Environment as envs } from '@/infra/web/nestjs/environment'
-import IRangoOrderService from '@/infra/persistence/service/irango-order.service';*/
+import IRangoOrderService from '@/infra/persistence/service/irango-order.service';
 
 describe("Test IRangoOrderService class", () => {
-    it("constructor class test", async () => {
+    let service:IRangoOrderService
+    let mockPost:jest.Mock<any>
 
+    beforeEach(() => {
+      jest.mock('axios')
+      mockPost = jest.fn()
+      axios.post = mockPost
+      service = new IRangoOrderService()
+    })
+
+    it("constructor class test", async () => {
+        expect(service).toBeInstanceOf(IRangoOrderService);
     });
 
-    it("confirmPayment method test", async () => {
+    it("confirmPayment method test without failing", async () => {
+        const pedidoId = 1;
+        const url = `${envs.SERVICE_IRANGO_ORDER_API}/v1/pedidos/payment-webhook/confirm/${pedidoId}`
+        mockPost.mockImplementation(() => {})
+        await service.confirmPayment(pedidoId);
+        expect(mockPost).toHaveBeenCalledTimes(1);
+        expect(mockPost).toHaveBeenCalledWith(url);
+    });
 
+    it("confirmPayment method test with failing", async () => {
+        const pedidoId = 1;
+        const url = `${envs.SERVICE_IRANGO_ORDER_API}/v1/pedidos/payment-webhook/confirm/${pedidoId}`
+        mockPost.mockImplementation(() => {
+            throw new Error('Mocked Error')
+        })
+        await service.confirmPayment(pedidoId);
+        expect(mockPost).toHaveBeenCalledTimes(1);
+        expect(mockPost).toHaveBeenCalledWith(url);
     });
 });
--- test/unit/src/infra/persistence/service/mercado-pago-gateway-pagamento.service.unit-spec.ts
+++ test/unit/src/infra/persistence/service/mercado-pago-gateway-pagamento.service.unit-spec.ts
@@ -1,15 +1,21 @@
-/*import { v4 as uuidv4 } from 'uuid'
-
 import Pedido from '@/core/domain/entities/pedido'
-import IGatewayPagamentoService from '@/core/domain/services/igateway-pagamento.service'
-import MercadoPagoGatewayPagamentoService from '@/infra/persistence/service/mercado-pago-gateway-pagamento.service';*/
+import MercadoPagoGatewayPagamentoService from '@/infra/persistence/service/mercado-pago-gateway-pagamento.service';
 
 describe("MercadoPagoGatewayPagamentoService class tests", () => {
-    it("class constructor test", async () => {
 
+    let gateway:MercadoPagoGatewayPagamentoService;
+
+    beforeEach(() => {
+        gateway = new MercadoPagoGatewayPagamentoService();
+    });
+
+    it("class constructor test", async () => {
+        expect(gateway).toBeInstanceOf(MercadoPagoGatewayPagamentoService);
     });
 
     it("class register method test", async () => {
-
+        let pedido = new Pedido();
+        let id = gateway.registerOrder(pedido);
+        expect(id).not.toBeNull();
     });
 });
