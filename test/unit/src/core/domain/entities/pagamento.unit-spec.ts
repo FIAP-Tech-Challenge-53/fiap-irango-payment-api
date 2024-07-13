c5e1f78aea0580e62544d2ae8dd51d427c4de4ee
Caio <caio.cb.sp@gmail.com>
2024-07-13 16:58:28 -0300

    Unit Tests for Core Entities Classes

--- test/unit/src/core/domain/entities/pagamento.unit-spec.ts
+++ test/unit/src/core/domain/entities/pagamento.unit-spec.ts
@@ -1,19 +1,46 @@
-/*import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 import { PagamentoStatusEnum } from '@/core/domain/enums/pagamento-status.enum'
 
-import Pagamento from '@/core/domain/entities/pagamento';*/
+import Pagamento from '@/core/domain/entities/pagamento';
 
 describe("Tests for Pagamento class", () => {
-    it("class constructor test", async () => {
+    it("class constructor test", () => {
+        let param = {
+            id: '1',
+            pedidoId: 1,
+            valor: 1,
+            gatewayPagamentoId: '1',
+            status: PagamentoStatusEnum.CONFIRMADO
+        }
 
+        let pagamento = new Pagamento(param);
+
+        expect(pagamento).toBeInstanceOf(Pagamento);
     });
 
-    it("class create test", async () => {
+    it("class create test", () => {
+        let pagamento = Pagamento.create(
+            1,
+            1,
+            '1',
+            PagamentoStatusEnum.CONFIRMADO
+        );
 
+        expect(pagamento).toBeInstanceOf(Pagamento);
     });
 
-    it("class update test", async () => {
+    it("class update test", () => {
+        let pagamento = Pagamento.create(
+            1,
+            1,
+            '1',
+            PagamentoStatusEnum.CANCELADO
+        );
 
+        Pagamento.update(pagamento, PagamentoStatusEnum.CONFIRMADO)
+
+        expect(pagamento).toBeInstanceOf(Pagamento);
+        expect(pagamento.status).toEqual(PagamentoStatusEnum.CONFIRMADO);
     });
 });
--- test/unit/src/core/domain/entities/pedido.unit-spec.ts
+++ test/unit/src/core/domain/entities/pedido.unit-spec.ts
@@ -1,11 +1,21 @@
-//import Pedido from "@/core/domain/entities/pedido";
+import Pedido from "@/core/domain/entities/pedido";
 
 describe("Pedido class tests", () => {
     it("constructor class test", async () => {
+        let param = {
+            id: 1,
+            consumidorId: '1',
+            total: 1,
+            pagamentoId: '1'
+        };
 
+        let pedido = new Pedido(param);
+
+        expect(pedido).toBeInstanceOf(Pedido);
     });
 
     it("create class test", async () => {
-
+        let pedido = Pedido.create(1, '1', 1, new Date(1), new Date(1));
+        expect(pedido).toBeInstanceOf(Pedido);
     });
 });
