0b3f3cdd6428b6a5239bf8fc7e9ecc78189a1acf
Caio <caio.cb.sp@gmail.com>
2024-07-13 23:24:53 -0300

    infra repositories class unit tests

--- test/unit/src/infra/persistence/typeorm/repository/pagamento-typeorm.repository.unit-spec.ts
+++ test/unit/src/infra/persistence/typeorm/repository/pagamento-typeorm.repository.unit-spec.ts
@@ -1,26 +1,135 @@
-/*import { Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 
 import PagamentoDto from '@/core/domain/dto/output/pagamento.dto'
 import Pagamento from '@/core/domain/entities/pagamento'
 import PagamentoMapper from '@/core/domain/mappers/pagamento.mapper'
-import IPagamentoRepository from '@/core/domain/repositories/ipagamento.repository'
 import { Pagamento as Entity } from '@/infra/persistence/typeorm/entities/pagamento'
-import PagamentoTypeormRepository from '@/infra/persistence/typeorm/repository/pagamento-typeorm.repository'*/
+import PagamentoTypeormRepository from '@/infra/persistence/typeorm/repository/pagamento-typeorm.repository'
+import { PagamentoStatusEnum } from '@/core/domain/enums/pagamento-status.enum'
 
 describe("PagamentoTypeormRepository class test", () => {
-    it("constructor class test", async () => {
 
+    let pagamentoTypeormRepository:PagamentoTypeormRepository
+
+    let repository:jest.Mocked<Repository<Entity>>
+  
+    let mockToDto:jest.Mock<any>
+    let toDomainEntity:jest.Mock<any>
+  
+    beforeEach(() => {
+      mockToDto = jest.fn()
+      toDomainEntity = jest.fn()
+  
+      repository = {
+        save: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn()
+      } as unknown as jest.Mocked<Repository<Entity>>
+  
+      PagamentoMapper.toDto = mockToDto
+      PagamentoMapper.toDomainEntity = toDomainEntity
+  
+      pagamentoTypeormRepository = new PagamentoTypeormRepository(repository)
+    })
+
+    it("constructor class test", async () => {
+        expect(pagamentoTypeormRepository).toBeInstanceOf(PagamentoTypeormRepository);
     });
 
     it("create method test", async () => {
+        let dto:PagamentoDto = {
+            id: '1',
+            pedidoId: 1,
+            valor: 1,
+            gatewayPagamentoId: '1',
+            status: PagamentoStatusEnum.CANCELADO,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        };
 
+        let pagamento = new Pagamento();
+
+        let entity = new Entity();
+        entity.pedidoId = 1;
+        entity.valor = 1;
+        entity.gatewayPagamentoId = '1';
+        entity.status = PagamentoStatusEnum.CANCELADO;
+        entity.createdAt = new Date(1);
+        entity.updatedAt = new Date(1);
+
+        mockToDto.mockReturnValue(dto);
+        toDomainEntity.mockReturnValue(pagamento);
+        repository.save.mockResolvedValue(entity);
+
+        let result = await pagamentoTypeormRepository.create(pagamento);
+        expect(mockToDto).toHaveBeenCalledTimes(1);
+        expect(toDomainEntity).toHaveBeenCalledTimes(1);
+        expect(repository.save).toHaveBeenCalledTimes(1);
+        expect(mockToDto).toHaveBeenCalledWith(pagamento);
+        expect(toDomainEntity).toHaveBeenCalledWith(entity);
+        expect(repository.save).toHaveBeenCalledWith(dto);
+        expect(result).toEqual(pagamento);
     });
 
     it("findByPedidoId method test", async () => {
 
+        let pagamento = new Pagamento();
+
+        let entity = new Entity();
+        entity.pedidoId = 1;
+        entity.valor = 1;
+        entity.gatewayPagamentoId = '1';
+        entity.status = PagamentoStatusEnum.CANCELADO;
+        entity.createdAt = new Date(1);
+        entity.updatedAt = new Date(1);
+
+        toDomainEntity.mockReturnValue(pagamento);
+        repository.findOne.mockResolvedValue(entity);
+
+        let result = await pagamentoTypeormRepository.findByPedidoId(1);
+
+        expect(toDomainEntity).toHaveBeenCalledTimes(1);
+        expect(repository.findOne).toHaveBeenCalledTimes(1);
+
+        expect(toDomainEntity).toHaveBeenCalledWith(entity);
+        expect(repository.findOne).toHaveBeenCalledWith({ where: { pedidoId: 1 } });
+
+        expect(result).toEqual(pagamento);
     });
 
     it("save method test", async () => {
+        let dto:PagamentoDto = {
+            id: '1',
+            pedidoId: 1,
+            valor: 1,
+            gatewayPagamentoId: '1',
+            status: PagamentoStatusEnum.CANCELADO,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        };
 
+        let pagamento = new Pagamento();
+
+        let entity = new Entity();
+        entity.pedidoId = 1;
+        entity.valor = 1;
+        entity.gatewayPagamentoId = '1';
+        entity.status = PagamentoStatusEnum.CANCELADO;
+        entity.createdAt = new Date(1);
+        entity.updatedAt = new Date(1);
+
+        mockToDto.mockReturnValue(dto);
+        toDomainEntity.mockReturnValue(pagamento);
+        repository.save.mockResolvedValue(entity);
+
+        let result = await pagamentoTypeormRepository.save(pagamento);
+        expect(mockToDto).toHaveBeenCalledTimes(1);
+        expect(toDomainEntity).toHaveBeenCalledTimes(1);
+        expect(repository.save).toHaveBeenCalledTimes(1);
+        expect(mockToDto).toHaveBeenCalledWith(pagamento);
+        expect(toDomainEntity).toHaveBeenCalledWith(entity);
+        expect(repository.save).toHaveBeenCalledWith(dto);
+        expect(result).toEqual(pagamento);
     });
 });
--- test/unit/src/infra/persistence/typeorm/repository/pedido-typeorm.repository.unit-spec.ts
+++ test/unit/src/infra/persistence/typeorm/repository/pedido-typeorm.repository.unit-spec.ts
@@ -1,22 +1,104 @@
-/*import { Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 
 import PedidoDto from '@/core/domain/dto/output/pedido.dto'
 import Pedido from '@/core/domain/entities/pedido'
 import PedidoMapper from '@/core/domain/mappers/pedido.mapper'
-import IPedidoRepository from '@/core/domain/repositories/ipedido.repository'
 import { Pedido as Entity } from '@/infra/persistence/typeorm/entities/pedido'
-import PedidoTypeormRepository from '@/infra/persistence/typeorm/repository/pedido-typeorm.repository'*/
+import PedidoTypeormRepository from '@/infra/persistence/typeorm/repository/pedido-typeorm.repository'
 
 describe("PedidoTypeormRepository class tests", () => {
-    it("constructor class test", async () => {
 
+    let pedidoTypeormRepository:PedidoTypeormRepository
+
+    let repository:jest.Mocked<Repository<Entity>>
+  
+    let mockToDto:jest.Mock<any>
+    let toDomainEntity:jest.Mock<any>
+  
+    beforeEach(() => {
+      mockToDto = jest.fn()
+      toDomainEntity = jest.fn()
+  
+      repository = {
+        save: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn()
+      } as unknown as jest.Mocked<Repository<Entity>>
+  
+      PedidoMapper.toDto = mockToDto
+      PedidoMapper.toDomainEntity = toDomainEntity
+  
+      pedidoTypeormRepository = new PedidoTypeormRepository(repository)
+    })
+
+    it("constructor class test", async () => {
+        expect(pedidoTypeormRepository).toBeInstanceOf(PedidoTypeormRepository);
     });
 
     it("create method test", async () => {
+        let dto:PedidoDto = {
+            id: 1,
+            consumidorId: '1',
+            pagamentoId: '1',
+            total: 1,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        };
 
+        let pedido = new Pedido();
+
+        let entity = new Entity();
+        entity.consumidorId = '1';
+        entity.pagamentoId = '1';
+        entity.total = 1;
+        entity.createdAt = new Date(1);
+        entity.updatedAt = new Date(1);
+
+        mockToDto.mockReturnValue(dto);
+        toDomainEntity.mockReturnValue(pedido);
+        repository.save.mockResolvedValue(entity);
+
+        let result = await pedidoTypeormRepository.create(pedido);
+        expect(mockToDto).toHaveBeenCalledTimes(1);
+        expect(toDomainEntity).toHaveBeenCalledTimes(1);
+        expect(repository.save).toHaveBeenCalledTimes(1);
+        expect(mockToDto).toHaveBeenCalledWith(pedido);
+        expect(toDomainEntity).toHaveBeenCalledWith(entity);
+        expect(repository.save).toHaveBeenCalledWith(dto);
+        expect(result).toEqual(pedido);
     });
 
     it("save method test", async () => {
+        let dto:PedidoDto = {
+            id: 1,
+            consumidorId: '1',
+            pagamentoId: '1',
+            total: 1,
+            createdAt: new Date(1),
+            updatedAt: new Date(1)
+        };
 
+        let pedido = new Pedido();
+
+        let entity = new Entity();
+        entity.consumidorId = '1';
+        entity.pagamentoId = '1';
+        entity.total = 1;
+        entity.createdAt = new Date(1);
+        entity.updatedAt = new Date(1);
+
+        mockToDto.mockReturnValue(dto);
+        toDomainEntity.mockReturnValue(pedido);
+        repository.save.mockResolvedValue(entity);
+
+        let result = await pedidoTypeormRepository.create(pedido);
+        expect(mockToDto).toHaveBeenCalledTimes(1);
+        expect(toDomainEntity).toHaveBeenCalledTimes(1);
+        expect(repository.save).toHaveBeenCalledTimes(1);
+        expect(mockToDto).toHaveBeenCalledWith(pedido);
+        expect(toDomainEntity).toHaveBeenCalledWith(entity);
+        expect(repository.save).toHaveBeenCalledWith(dto);
+        expect(result).toEqual(pedido);
     });
 });
